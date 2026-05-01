3c0e3ea4d4d1f94aaffff8d0c6adb0de17b55446
aathikanm <aathikanm@gmail.com>
2026-05-01 22:03:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-let text = "IT Student | AI Engineer | Full Stack Developer";
-let i = 0;
-
-function typing() {
-    if (i < text.length) {
-        document.getElementById("typing").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typing, 60);
-    }
-}
-typing();
-
 function toggleDark() {
-    document.body.classList.toggle("dark");
-}
-
-function msg() {
-    alert("Thanks for visiting my portfolio 🚀");
+  document.body.classList.toggle("dark");
 }
